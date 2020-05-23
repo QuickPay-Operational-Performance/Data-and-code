dd106411260c5c392da6ef5ad308b50f67ba1cd4
Vibhuti Dhingra <47764977+vibhuti6@users.noreply.github.com>
2020-05-22 18:25:48 -0700

    code for obtaining list of duns number to match with Mergent database

---
+++ codes/duns_number.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 22 17:57:53 2020
+
+@author: vibhutidhingra
+"""
+
+
+import pandas as pd
+import glob, os
+
+pd.set_option('display.expand_frame_repr', False)
+
+path_to_folder='/Users/vibhutidhingra/Dropbox/data_quickpay/qp_data'
+col_names=['contract_award_unique_key','recipient_duns',\
+           'recipient_name', 'contracting_officers_determination_of_business_size']
+
+all_files = glob.glob(os.path.join(path_to_folder, "*.csv"))  
+# create a list of path for each file in the folder
+df_from_each_file = (pd.read_csv(f,usecols=col_names) for f in all_files)
+# query each file in the folder and save as generator
+df = pd.concat(df_from_each_file, ignore_index=True)
+# merge into one dataset
+df_unique=df.drop_duplicates(subset='contract_award_unique_key').reset_index()
+# drop duplicates
+df_unique.recipient_duns=df_unique.recipient_duns.astype('Int64')
+# convert duns number to integer type 
+df_unique.to_csv('/Users/vibhutidhingra/Dropbox/data_quickpay/full_sample_recipient_duns.csv',index=False)
+# save to csv 
+
+# example rows/columns
+df_example=df_unique[['recipient_duns','recipient_name','contracting_officers_determination_of_business_size']].iloc[0:5]
+print(df_example.to_markdown(showindex=False).replace('\n',''))
