f59fcd4a31bff65cc76fe18e323c2e28cbb11715
Vibhuti Dhingra <47764977+vibhuti6@users.noreply.github.com>
2020-06-27 02:10:50 -0700

    plots and update on share of govt on sales

--- codes/summary_stats.py
+++ codes/summary_stats.py
@@ -6,6 +6,7 @@ Created on Sat Jun 20 14:35:19 2020
 @author: vibhutidhingra
 """
 import pandas as pd 
+import matplotlib.pyplot as plt
 
 pd.set_option('expand_frame_repr', True)
 
@@ -47,19 +48,114 @@ lb=df[df.contracting_officers_determination_of_business_size_code=='O']
 lb_summary_1=lb.groupby(['action_date_year_quarter'])['recipient_duns','contract_award_unique_key'].nunique().reset_index()
 lb_summary_1.rename(columns={'recipient_duns':'Number of firms','contract_award_unique_key':'Number of contracts'},inplace=True)
 
-pba=lb[lb.performance_based_service_acquisition_code=='Y'].groupby(['action_date_year_quarter'])['contract_award_unique_key'].nunique().reset_index()
-pba=pba.rename(columns={'contract_award_unique_key':'Number of performance based contracts'})
+pba_lb=lb[lb.performance_based_service_acquisition_code=='Y'].groupby(['action_date_year_quarter'])['contract_award_unique_key'].nunique().reset_index()
+pba_lb=pba_lb.rename(columns={'contract_award_unique_key':'Number of performance based contracts'})
 
-fin=lb[lb.contract_financing_code.isin(['C','A','D','E','F'])].groupby(['action_date_year_quarter'])['contract_award_unique_key'].nunique().reset_index()
-fin=fin.rename(columns={'contract_award_unique_key':'Number of contracts receiving financing'})
+fin_lb=lb[lb.contract_financing_code.isin(['C','A','D','E','F'])].groupby(['action_date_year_quarter'])['contract_award_unique_key'].nunique().reset_index()
+fin_lb=fin_lb.rename(columns={'contract_award_unique_key':'Number of contracts receiving financing'})
 
-non_competitive=lb[lb.extent_competed_code.isin(['G','B','C'])].groupby(['action_date_year_quarter'])['contract_award_unique_key'].nunique().reset_index()
-non_competitive=non_competitive.rename(columns={'contract_award_unique_key':'Number of non-competed contracts'})
+non_competitive_lb=lb[lb.extent_competed_code.isin(['G','B','C'])].groupby(['action_date_year_quarter'])['contract_award_unique_key'].nunique().reset_index()
+non_competitive_lb=non_competitive_lb.rename(columns={'contract_award_unique_key':'Number of non-competed contracts'})
 
-dfs = [df.set_index(['action_date_year_quarter']) for df in [lb_summary_1, pba, fin,non_competitive]]
+dfs = [df.set_index(['action_date_year_quarter']) for df in [lb_summary_1, pba_lb, fin_lb,non_competitive_lb]]
 lb_summary=pd.concat(dfs, axis=1).reset_index()
 
 #%% Write to md
 
 # 'wt' -- overwrite existing material, 'a' -- append to existing material                
-print(lb_summary.to_markdown(), file=open('~/Desktop/lb_summary_stats.md','wt'))  
+print(lb_summary.to_markdown(), file=open('/Users/vibhutidhingra/Desktop/lb_summary_stats.md','wt'))  
+
+
+#%% Number of null values (convert to string to show in groupby)
+
+cols=['performance_based_service_acquisition','performance_based_service_acquisition_code',\
+      'extent_competed','extent_competed_code',\
+      'contract_financing','contract_financing_code']
+df[cols]=df[cols].astype(str)
+
+## pba
+pba_notnull=df.groupby(['performance_based_service_acquisition',\
+                        'performance_based_service_acquisition_code'])['contract_award_unique_key'].nunique().reset_index()
+pba_notnull=pba_notnull.rename(columns={'contract_award_unique_key':'Number of contracts'})
+
+## extent competed
+noncompete_notnull=df.groupby(['extent_competed','extent_competed_code'])['contract_award_unique_key'].nunique().reset_index()
+noncompete_notnull=noncompete_notnull.rename(columns={'contract_award_unique_key':'Number of contracts'})
+
+## contract financing
+fin_notnull=df.groupby(['contract_financing','contract_financing_code'])['contract_award_unique_key'].nunique().reset_index()
+fin_notnull=fin_notnull.rename(columns={'contract_award_unique_key':'Number of contracts'})
+
+## to markdown
+
+print(pba_notnull.to_markdown(), file=open('/Users/vibhutidhingra/Desktop/pba_notnull.md','wt'))  
+print(noncompete_notnull.to_markdown(), file=open('/Users/vibhutidhingra/Desktop/noncompete_notnull.md','wt'))  
+print(fin_notnull.to_markdown(), file=open('/Users/vibhutidhingra/Desktop/fin_notnull.md','wt'))  
+
+#%% Plot the total number of firms over time
+folder_path='/Users/vibhutidhingra/Desktop/research/Git:Github/qp_data_and_code/img/summary_stats'
+fig = plt.figure(figsize=(8, 6))
+plt.xticks(rotation=90)
+plt.plot(lb_summary.action_date_year_quarter,lb_summary['Number of firms'],\
+         label='Large Business')
+plt.plot(sb_summary.action_date_year_quarter,sb_summary['Number of firms'],\
+         label='Small Business')
+fig.suptitle('Number of firms', fontsize=18)
+plt.legend(loc="best")
+#plt.ylabel('Fraction of contracts', fontsize=18)
+plt.xlabel('Year-Quarter', fontsize=16)
+plt.savefig(folder_path+'/summary_number_firms.png',bbox_inches='tight')
+#%% Plot the total number of contracts over time
+
+fig = plt.figure(figsize=(8, 6))
+plt.xticks(rotation=90)
+plt.plot(lb_summary.action_date_year_quarter,lb_summary['Number of contracts'],\
+         label='Large Business')
+plt.plot(sb_summary.action_date_year_quarter,sb_summary['Number of contracts'],\
+         label='Small Business')
+fig.suptitle('Number of unique active contracts', fontsize=18)
+plt.legend(loc="best")
+#plt.ylabel('Fraction of contracts', fontsize=18)
+plt.xlabel('Year-Quarter', fontsize=16)
+plt.savefig(folder_path+'/summary_number_contracts.png',bbox_inches='tight')
+#%% Plot the graph - Performance based
+
+fig = plt.figure(figsize=(8, 6))
+plt.xticks(rotation=90)
+plt.plot(lb_summary.action_date_year_quarter,lb_summary['Number of performance based contracts']/lb_summary['Number of contracts'],\
+         label='Large Business')
+plt.plot(sb_summary.action_date_year_quarter,sb_summary['Number of performance based contracts']/sb_summary['Number of contracts'],\
+         label='Small Business')
+fig.suptitle('Performance-based contracts', fontsize=18)
+plt.legend(loc="best")
+plt.ylabel('Fraction of contracts', fontsize=18)
+plt.xlabel('Year-Quarter', fontsize=16)
+plt.savefig(folder_path+'/summary_pba.png',bbox_inches='tight')
+#%% Plot the graph - Financing
+
+fig = plt.figure(figsize=(8, 6))
+plt.xticks(rotation=90)
+plt.plot(lb_summary.action_date_year_quarter,lb_summary['Number of contracts receiving financing']/lb_summary['Number of contracts'],\
+         label='Large Business')
+plt.plot(sb_summary.action_date_year_quarter,sb_summary['Number of contracts receiving financing']/sb_summary['Number of contracts'],\
+         label='Small Business')
+fig.suptitle('Financed contracts', fontsize=18)
+plt.legend(loc="best")
+plt.ylabel('Fraction of contracts', fontsize=18)
+plt.xlabel('Year-Quarter', fontsize=16)
+plt.savefig(folder_path+'/summary_contract_financing.png',bbox_inches='tight')
+#%% Plot the graph - Non-competed contracts
+
+fig = plt.figure(figsize=(8, 6))
+plt.xticks(rotation=90)
+plt.plot(lb_summary.action_date_year_quarter,\
+         lb_summary['Number of non-competed contracts']/lb_summary['Number of contracts'],\
+         label='Large Business')
+plt.plot(sb_summary.action_date_year_quarter,\
+         sb_summary['Number of non-competed contracts']/sb_summary['Number of contracts'],\
+         label='Small Business')
+fig.suptitle('Non-competitive contracts', fontsize=18)
+plt.legend(loc="best")
+plt.ylabel('Fraction of contracts', fontsize=18)
+plt.xlabel('Year-Quarter', fontsize=16)
+plt.savefig(folder_path+'/summary_non_compete.png',bbox_inches='tight')
--- codes/weight_of_govt_contracts.R
+++ codes/weight_of_govt_contracts.R
@@ -36,72 +36,292 @@ sales_df[ , ("value") := lapply(.SD,  function(x) as.numeric(gsub("[,$]", "", x)
 
 sales_df[, sales_year:=as.numeric(substr(variable, start = 1, stop = 4))]
 
+# the years in Intellect data correspond to fiscal years 
+
 #### Read USASpending data #### 
 
 dft=fread('/Users/vibhutidhingra/Dropbox/data_quickpay/qp_data/qp_data_fy10_to_fy18.csv',
          select = c("contract_award_unique_key",
                     "recipient_duns",
                     "federal_action_obligation",
-                    "action_date",
+                    "action_date_fiscal_year",
                     "contracting_officers_determination_of_business_size_code"))
 
 dft=dft[contracting_officers_determination_of_business_size_code%in%c("O","S")]
-
-dft[, action_date:=as.Date(action_date)]
-dft[, action_date_calendar_year:=as.numeric(format(action_date,'%Y'))]
 dft[, recipient_duns:=as.numeric(recipient_duns)]
 
 # group by recipient duns and year, and add all federal action obligations
 fao=dft[, 
             lapply(.SD, sum),
             .SDcols = c("federal_action_obligation"),
-            by = c("recipient_duns","action_date_calendar_year")] 
+            by = c("recipient_duns","action_date_fiscal_year")] 
 
 setnames(fao, c("federal_action_obligation"), 
-         c("sum_federal_action_obligation"))
+         c("sum_of_federal_action_obligation"))
 
 #### Merge the two datasets ####
 govt_weight_per_recipient=merge(x=fao,y=sales_df,
-      by.x=c("recipient_duns","action_date_calendar_year"),
+      by.x=c("recipient_duns","action_date_fiscal_year"),
       by.y=c("D-U-N-S@ Number","sales_year"))[]
 
 setnames(govt_weight_per_recipient, c("variable","value"), 
-         c("sales_year","sales_volume"))
+         c("sales_fiscal_year","sales_volume"))
 
-govt_weight_per_recipient[, fao_weight:= ifelse(is.finite(sum_federal_action_obligation) & sales_volume>0,
-                                                sum_federal_action_obligation/sales_volume, NaN)]
+govt_weight_per_recipient[, fao_weight:= ifelse(is.finite(sum_of_federal_action_obligation) &
+                                                  is.finite(sales_volume)&
+                                                  sales_volume>0,
+                                                sum_of_federal_action_obligation/sales_volume, NaN)]
                             
 #### get contracts info #### 
 
-# get unique values based on contract ID and year
-dft_contracts_only=unique(dft[,c("contract_award_unique_key",
-                "action_date_calendar_year",
-                "recipient_duns",
-                "contracting_officers_determination_of_business_size_code")],
-         by=c("contract_award_unique_key",
-                "action_date_calendar_year"))
+# get list of firms active in a given year and their contract classification
+# eg. suppose firm A appears in 2015 as both S & O
+# then we will have two entries for above: 1) A, 2015, S and 2) A, 2015, O
 
-# merge with sales info
-contracts_share_of_govt=merge(dft_contracts_only,govt_weight_per_recipient,
-                         by=c("recipient_duns","action_date_calendar_year"))[]
+fao_weight_table=unique(dft[,c("recipient_duns",
+                "action_date_fiscal_year",
+                "contracting_officers_determination_of_business_size_code")])
+
+
+fao_weight_table=merge(x=fao_weight_table,y=govt_weight_per_recipient,
+                                by=c("recipient_duns","action_date_fiscal_year"))[]
 
 #### summary #### 
 
 # summarize based on small and large businesses
-fao_weight_summary=contracts_share_of_govt[, 
-        lapply(.SD, mean, na.rm=TRUE),
-        .SDcols = c("sum_federal_action_obligation",
-                    "sales_volume"),
-        by = c("action_date_calendar_year",
+fao_weight_median=fao_weight_table[, 
+        lapply(.SD, median, na.rm=TRUE),
+        .SDcols = c("sum_of_federal_action_obligation",
+                    "sales_volume",
+                    "fao_weight"),
+        by = c("action_date_fiscal_year",
                "contracting_officers_determination_of_business_size_code")] 
-setnames(fao_weight_summary, c("sum_federal_action_obligation","sales_volume"), 
-         c("mean_total_obligations","mean_sales_volume"))
 
-fao_weight_summary[,"obligations_to_sales_ratio":=mean_total_obligations/mean_sales_volume]
+setnames(fao_weight_median, c("sum_of_federal_action_obligation","sales_volume","fao_weight"), 
+         c("median_total_obligations","median_sales_volume","median_fao_weight"))
 
-fao_weight_summary=fao_weight_summary[order(action_date_calendar_year)]
-kable(fao_weight_summary)
+fao_weight_mean=fao_weight_table[, 
+                                   lapply(.SD, mean, na.rm=TRUE),
+                                   .SDcols = c("sum_of_federal_action_obligation",
+                                               "sales_volume",
+                                               "fao_weight"),
+                                   by = c("action_date_fiscal_year",
+                                          "contracting_officers_determination_of_business_size_code")] 
+
+setnames(fao_weight_mean, c("sum_of_federal_action_obligation","sales_volume","fao_weight"), 
+         c("mean_total_obligations","mean_sales_volume","mean_fao_weight"))
+
+fao_weight_summary=merge(fao_weight_mean, fao_weight_median,
+                         by=c("action_date_fiscal_year",
+                              "contracting_officers_determination_of_business_size_code"))
+
+fao_weight_summary[,action_date_fiscal_year:=as.factor(action_date_fiscal_year)]
+setnames(fao_weight_summary,"contracting_officers_determination_of_business_size_code","business_size")
 
 
+fao_weight_summary=fao_weight_summary[order(action_date_fiscal_year)]
+#kable(fao_weight_summary)
 
+#### mean plots ####
+
+mean_weight_plot=ggplot(fao_weight_summary, 
+       aes(x=action_date_fiscal_year, 
+           y=mean_fao_weight,
+           shape=business_size, group=1)) +
+          geom_point(size=3)+ 
+          theme_bw()+
+          theme(panel.grid.major = element_blank(),
+                panel.grid.minor = element_blank()) 
+
+mean_sales_plot=ggplot(fao_weight_summary, 
+       aes(x=action_date_fiscal_year, 
+           y=mean_sales_volume,
+           shape=business_size, group=1)) +
+            geom_point(size=3)+ 
+            theme_bw()+
+            theme(panel.grid.major = element_blank(),
+                  panel.grid.minor = element_blank()) 
+
+mean_obligations_plot=ggplot(fao_weight_summary, 
+       aes(x=action_date_fiscal_year, 
+           y=mean_total_obligations,
+           shape=business_size, group=1)) +
+            geom_point(size=3)+ 
+            theme_bw()+
+            theme(panel.grid.major = element_blank(),
+                  panel.grid.minor = element_blank()) 
+
+#### median plots ####
+
+median_weight_plot=ggplot(fao_weight_summary, 
+                        aes(x=action_date_fiscal_year, 
+                            y=median_fao_weight,
+                            shape=business_size, group=1)) +
+                    geom_point(size=3)+ 
+                    theme_bw()+
+                    theme(panel.grid.major = element_blank(),
+                          panel.grid.minor = element_blank()) 
+median_sales_plot=ggplot(fao_weight_summary, 
+                       aes(x=action_date_fiscal_year, 
+                           y=median_sales_volume,
+                           shape=business_size, group=1)) +
+                      geom_point(size=3)+ 
+                      theme_bw()+
+                      theme(panel.grid.major = element_blank(),
+                            panel.grid.minor = element_blank()) 
+median_obligations_plot=ggplot(fao_weight_summary, 
+                             aes(x=action_date_fiscal_year, 
+                                 y=median_total_obligations,
+                                 shape=business_size, group=1)) +
+                      geom_point(size=3)+ 
+                      theme_bw()+
+                      theme(panel.grid.major = element_blank(),
+                            panel.grid.minor = element_blank()) #+ggtitle("Plant growth")
+
+#### Analysis for firms with NO negative obligations ####
+
+negative_obligation_firms=unique(dft[federal_action_obligation<0]$recipient_duns)
+dft_non_neg_fao=dft[!recipient_duns%in%negative_obligation_firms]
+
+# group by recipient duns and year, and add all federal action obligations
+fao_non_neg_fao=dft_non_neg_fao[, 
+        lapply(.SD, sum),
+        .SDcols = c("federal_action_obligation"),
+        by = c("recipient_duns","action_date_fiscal_year")] 
+
+setnames(fao_non_neg_fao, c("federal_action_obligation"), 
+         c("sum_of_federal_action_obligation"))
+
+#### NO negative obligations: Merge the two datasets ####
+govt_weight_per_recipient_non_neg_fao=merge(x=fao_non_neg_fao,y=sales_df,
+                                by.x=c("recipient_duns","action_date_fiscal_year"),
+                                by.y=c("D-U-N-S@ Number","sales_year"))[]
+
+setnames(govt_weight_per_recipient_non_neg_fao, c("variable","value"), 
+         c("sales_fiscal_year","sales_volume"))
+
+govt_weight_per_recipient_non_neg_fao[, fao_weight:= ifelse(is.finite(sum_of_federal_action_obligation) &
+                                                  is.finite(sales_volume)&
+                                                  sales_volume>0,
+                                                sum_of_federal_action_obligation/sales_volume, NaN)]
+
+#### NO negative obligations: get contracts info #### 
+
+# get list of firms active in a given year and their contract classification
+# eg. suppose firm A appears in 2015 as both S & O
+# then we will have two entries for above: 1) A, 2015, S and 2) A, 2015, O
+
+fao_weight_table_non_neg_fao=unique(dft_non_neg_fao[,c("recipient_duns",
+                               "action_date_fiscal_year",
+                               "contracting_officers_determination_of_business_size_code")])
+
+
+fao_weight_table_non_neg_fao=merge(x=fao_weight_table_non_neg_fao,
+                                   y=govt_weight_per_recipient_non_neg_fao,
+                       by=c("recipient_duns","action_date_fiscal_year"))[]
+
+#### NO negative obligations: summary #### 
+
+# summarize based on small and large businesses
+fao_weight_median_non_neg_fao=fao_weight_table_non_neg_fao[, 
+                                   lapply(.SD, median, na.rm=TRUE),
+                                   .SDcols = c("sum_of_federal_action_obligation",
+                                               "sales_volume",
+                                               "fao_weight"),
+                                   by = c("action_date_fiscal_year",
+                                          "contracting_officers_determination_of_business_size_code")] 
+
+setnames(fao_weight_median_non_neg_fao, c("sum_of_federal_action_obligation","sales_volume","fao_weight"), 
+         c("median_total_obligations","median_sales_volume","median_fao_weight"))
+
+fao_weight_mean_non_neg_fao=fao_weight_table_non_neg_fao[, 
+                                 lapply(.SD, mean, na.rm=TRUE),
+                                 .SDcols = c("sum_of_federal_action_obligation",
+                                             "sales_volume",
+                                             "fao_weight"),
+                                 by = c("action_date_fiscal_year",
+                                        "contracting_officers_determination_of_business_size_code")] 
+
+setnames(fao_weight_mean_non_neg_fao, c("sum_of_federal_action_obligation","sales_volume","fao_weight"), 
+         c("mean_total_obligations","mean_sales_volume","mean_fao_weight"))
+
+fao_weight_summary_non_neg_fao=merge(fao_weight_mean_non_neg_fao, fao_weight_median_non_neg_fao,
+                         by=c("action_date_fiscal_year",
+                              "contracting_officers_determination_of_business_size_code"))
+
+fao_weight_summary_non_neg_fao[,action_date_fiscal_year:=as.factor(action_date_fiscal_year)]
+setnames(fao_weight_summary_non_neg_fao,"contracting_officers_determination_of_business_size_code","business_size")
+
+
+fao_weight_summary_non_neg_fao=fao_weight_summary_non_neg_fao[order(action_date_fiscal_year)]
+#kable(fao_weight_summary)
+
+#### NO negative obligations: mean plots ####
+
+mean_weight_plot_non_neg_fao=ggplot(fao_weight_summary_non_neg_fao, 
+                        aes(x=action_date_fiscal_year, 
+                            y=mean_fao_weight,
+                            shape=business_size, group=1)) +
+  geom_point(size=3)+ 
+  theme_bw()+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank()) +
+  ggtitle("Only firms with non-negative obligations")
+
+mean_sales_plot_non_neg_fao=ggplot(fao_weight_summary_non_neg_fao, 
+                       aes(x=action_date_fiscal_year, 
+                           y=mean_sales_volume,
+                           shape=business_size, group=1)) +
+  geom_point(size=3)+ 
+  theme_bw()+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())  +
+  ggtitle("Only firms with non-negative obligations")
+
+
+mean_obligations_plot_non_neg_fao=ggplot(fao_weight_summary_non_neg_fao, 
+                             aes(x=action_date_fiscal_year, 
+                                 y=mean_total_obligations,
+                                 shape=business_size, group=1)) +
+  geom_point(size=3)+ 
+  theme_bw()+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())  +
+  ggtitle("Only firms with non-negative obligations")
+
+
+#### NO negative obligations: median plots ####
+
+median_weight_plot_non_neg_fao=ggplot(fao_weight_summary_non_neg_fao, 
+                          aes(x=action_date_fiscal_year, 
+                              y=median_fao_weight,
+                              shape=business_size, group=1)) +
+  geom_point(size=3)+ 
+  theme_bw()+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())  +
+  ggtitle("Only firms with non-negative obligations")
+
+
+median_sales_plot_non_neg_fao=ggplot(fao_weight_summary_non_neg_fao, 
+                         aes(x=action_date_fiscal_year, 
+                             y=median_sales_volume,
+                             shape=business_size, group=1)) +
+  geom_point(size=3)+ 
+  theme_bw()+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())  +
+  ggtitle("Only firms with non-negative obligations")
+
+median_obligations_plot_non_neg_fao=ggplot(fao_weight_summary_non_neg_fao, 
+                               aes(x=action_date_fiscal_year, 
+                                   y=median_total_obligations,
+                                   shape=business_size, group=1)) +
+  geom_point(size=3)+ 
+  theme_bw()+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())  +
+  ggtitle("Only firms with non-negative obligations")
+
+#ggsave(file="length-hist.png")
 
