27be23dd9893bfb0684f4be05e88c3deb48672f3
Vibhuti Dhingra <47764977+vibhuti6@users.noreply.github.com>
2020-06-28 18:59:28 -0700

    summary stats for contract value and length

--- codes/summary_stats.py
+++ codes/summary_stats.py
@@ -158,3 +158,50 @@ fin_notnull=fin_notnull.rename(columns={'contract_award_unique_key':'Number of c
 print(pba_notnull.to_markdown(), file=open('/Users/vibhutidhingra/Desktop/pba_notnull.md','wt'))  
 print(noncompete_notnull.to_markdown(), file=open('/Users/vibhutidhingra/Desktop/noncompete_notnull.md','wt'))  
 print(fin_notnull.to_markdown(), file=open('/Users/vibhutidhingra/Desktop/fin_notnull.md','wt'))  
+
+#%% Value and length of contracts 
+from scipy.stats import mstats
+folder_path='/Users/vibhutidhingra/Desktop/research/Git:Github/qp_data_and_code/img/summary_stats'
+
+dfv=df.sort_values(by='action_date').drop_duplicates(subset='contract_award_unique_key').reset_index(drop=True)
+# get entry when a contract first appeared in the sample
+
+dfv['period_of_performance_current_end_date']=pd.to_datetime(dfv.period_of_performance_current_end_date)
+dfv['period_of_performance_start_date']=pd.to_datetime(dfv.period_of_performance_start_date)
+
+dfv["initial_duration"]=(dfv.period_of_performance_current_end_date-dfv.period_of_performance_start_date).dt.days
+dfv["winsorized_contract_value"]=mstats.winsorize(dfv.base_and_all_options_value, limits=[0.05, 0.05])
+dfv["winsorized_initial_duration"]=mstats.winsorize(dfv.initial_duration, limits=[0.05, 0.05])
+
+sb_contracts=dfv[dfv.contracting_officers_determination_of_business_size_code=='S'].reset_index()
+lb_contracts=dfv[dfv.contracting_officers_determination_of_business_size_code=='O'].reset_index()
+
+plot_sb=sb_contracts.groupby('action_date_year_quarter')[['base_and_all_options_value',\
+                            'winsorized_contract_value','initial_duration','winsorized_initial_duration']].mean().reset_index()
+plot_lb=lb_contracts.groupby('action_date_year_quarter')[['base_and_all_options_value',\
+                            'winsorized_contract_value','initial_duration','winsorized_initial_duration']].mean().reset_index()
+
+fig = plt.figure(figsize=(8, 6))
+plt.xticks(rotation=90)
+plt.plot(plot_sb.action_date_year_quarter.astype(str),\
+         plot_sb.winsorized_contract_value,label="Small Business")
+plt.plot(plot_lb.action_date_year_quarter.astype(str),\
+         plot_lb.winsorized_contract_value,label="Large Business")
+plt.legend(loc="best")
+plt.ylabel('Initial contract value \n (5% winsorized average)', fontsize=18)
+plt.xlabel('Year-Quarter', fontsize=16)
+plt.savefig(folder_path+'/summary_initial_value.png',bbox_inches='tight')
+
+fig = plt.figure(figsize=(8, 6))
+plt.xticks(rotation=90)
+plt.plot(plot_sb.action_date_year_quarter.astype(str),\
+         plot_sb.winsorized_initial_duration,label="Small Business")
+plt.plot(plot_lb.action_date_year_quarter.astype(str),\
+         plot_lb.winsorized_initial_duration,label="Large Business")
+plt.legend(loc="best")
+plt.ylabel('Initial project duration \n (5% winsorized average)', fontsize=18)
+plt.xlabel('Year-Quarter', fontsize=16)
+plt.savefig(folder_path+'/summary_initial_duration.png',bbox_inches='tight')
+
+
+
