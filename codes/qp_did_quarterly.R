354797620a54554a0d8b874b4519ddb22bb2abe2
Vibhuti Dhingra <47764977+vibhuti6@users.noreply.github.com>
2020-07-15 02:56:33 -0700

    update regression for obligation to sales ratio

--- codes/qp_did_quarterly.R
+++ codes/qp_did_quarterly.R
@@ -32,7 +32,7 @@ df$after_quickpay=ifelse(df$action_date_year_quarter>as.Date("2011-04-27"),1,0)
 df$small_business=ifelse(df$business_type=="S",1,0)
 
 df$naics_code<-as.factor(df$naics_code)
-df$recipient_duns<-as.factor(df$recipient_duns)
+#df$recipient_duns<-as.factor(df$recipient_duns)
 df$product_or_service_code<-as.factor(df$product_or_service_code)
 
 #### Baseline Regressions ####
@@ -383,20 +383,140 @@ stargazer(no_fe_pba,task_fe_pba,task_and_industry_fe_pba,
           type="html",
           header=F)
 
+#### Terciles Obligation to Sales Ratio: Firms with one type of contract ####
+
+range = "Oct 2009 to June 2012"
+
+library(data.table)
+
+firms_with_multiple_types=unique(setDT(df)[,uniqueN(business_type),by=recipient_duns][V1==2,]$recipient_duns)
+
+df2=subset(setDT(df),!recipient_duns%in%firms_with_multiple_types)
+
+# add fiscal year to df2 (contract data)
+# format Add the year to 1 if the month (in action-date-year-quarter) 
+# is greater than or equal to 10 (or to zero if not)
+# because new fiscal year starts from Oct
+
+df2[,action_date_year_quarter:=as.Date(action_date_year_quarter)]
+df2[,action_date_fiscal_year:=as.numeric(format(action_date_year_quarter, "%Y")) 
+    + (format(action_date_year_quarter, "%m") >= "10")]
+
+# We are assuming fiscal years are same in USASpending & Intellect data
+
+fao_to_sales=fread('/Users/vibhutidhingra/Dropbox/data_quickpay/qp_data/govt_weight_per_recipient.csv')
+df3=merge(df2,fao_to_sales,by= c("recipient_duns","action_date_fiscal_year"))
+
+df3[,winsorized_fao_weight:=Winsorize(fao_weight,na.rm=TRUE)]
+df3[,fao_weight_tercile:=ntile(winsorized_fao_weight,3)]
+
+# Y = a + LargeBusiness + Before2014 + LargeBusiness x Before2014 + e
+
+select_tercile = 3
+
+tercile_name=case_when(select_tercile==1 ~ "Bottom Tercile",
+                       select_tercile==2 ~ "Middle Tercile",
+                       select_tercile==3 ~ "Top Tercile")
+
+no_fe=felm(winsorized_delay ~ after_quickpay*small_business |
+             0|0|0,     
+           data = subset(df3,fao_weight_tercile==select_tercile))
+
+task_fe=felm(winsorized_delay ~after_quickpay*small_business |
+               product_or_service_code|0|0,     
+             data = subset(df3,fao_weight_tercile==select_tercile))
+
+task_and_industry_fe=felm(winsorized_delay ~after_quickpay*small_business |
+                            naics_code+product_or_service_code|0|0,     
+                          data = subset(df3,fao_weight_tercile==select_tercile))
+
+stargazer(no_fe,task_fe,task_and_industry_fe,
+          title = paste("Days of Delay (Winsorized):",range, tercile_name, "of Obligation to Sales Ratio", sep=" "),
+          dep.var.labels.include = TRUE,
+          object.names=FALSE, 
+          model.numbers=FALSE,
+          add.lines = list(c("PSC code FE","No","Yes","Yes"),
+                           c("Industry FE","No","No","Yes"),
+                           c("Controls","No","No","No")), 
+          style="qje",
+          notes.align = "l",
+          notes=" (i) Each observation is a project-quarter, (ii) Sample restricted to firms that receive only one type of contract (small or large, but not both)",
+          type="html",
+          header=F)
+
+## Summary: 
+
+summary_fao_weight=na.omit(df3)[, max(winsorized_fao_weight), by=fao_weight_tercile][, .(fao_weight_tercile = fao_weight_tercile, 
+                                                                                         max_winsorized_fao_ratio = V1)]
+
+kableExtra::kable(summary_fao_weight)
+
+summary_fao_weight_num_contracts=na.omit(df3)[, uniqueN(contract_award_unique_key), by=list(fao_weight_tercile,business_type)][, .(fao_weight_tercile = fao_weight_tercile, 
+                                                                                                                                   business_type=business_type,
+                                                                                                                                   number_of_contracts = V1)][order(fao_weight_tercile)]
+kableExtra::kable(summary_fao_weight_num_contracts)
 
 
+#### Firms with one type of contract: Parametric Obligation to Sales Ratio ####
 
+no_fe=felm(winsorized_delay ~ after_quickpay*small_business*winsorized_fao_weight|
+             0|0|0,     
+           data = df3)
 
+task_fe=felm(winsorized_delay ~after_quickpay*small_business*winsorized_fao_weight|
+               product_or_service_code|0|0,     
+             data = df3)
 
+task_and_industry_fe=felm(winsorized_delay ~ after_quickpay*small_business*winsorized_fao_weight |
+                            naics_code+product_or_service_code|0|0,     
+                          data = df3)
 
+stargazer(no_fe,task_fe,task_and_industry_fe,
+          title = paste("Days of Delay (Winsorized):", range, sep=" "),
+          dep.var.labels.include = TRUE,
+          object.names=FALSE, 
+          model.numbers=FALSE,
+          add.lines = list(c("PSC code FE","No","Yes","Yes"),
+                           c("Industry FE","No","No","Yes"),
+                           c("Controls","No","No","No")), 
+          style="qje",
+          notes.align = "l",
+          notes=" (i) Each observation is a project-quarter, (ii) Sample restricted to firms that receive only one type of contract (small or large, but not both)",
+          type="html",
+          header=F)
 
+# Aliter 
 
+# Delay = b0 + b1 SmallBusiness + b2 AfterQP + b3 (SmallBusiness x AfterQP x FaoRatio) + e
 
+no_fe=felm(winsorized_delay ~ after_quickpay+small_business+
+             after_quickpay:small_business:winsorized_fao_weight|
+             0|0|0,     
+           data = df3)
 
+task_fe=felm(winsorized_delay ~after_quickpay+small_business+
+               after_quickpay:small_business:winsorized_fao_weight|
+               product_or_service_code|0|0,     
+             data = df3)
 
+task_and_industry_fe=felm(winsorized_delay ~ after_quickpay+small_business+
+                            after_quickpay:small_business:winsorized_fao_weight|
+                            naics_code+product_or_service_code|0|0,     
+                          data = df3)
 
-
-
+stargazer(no_fe,task_fe,task_and_industry_fe,
+          title = paste("Days of Delay (Winsorized):", range, sep=" "),
+          dep.var.labels.include = TRUE,
+          object.names=FALSE, 
+          model.numbers=FALSE,
+          add.lines = list(c("PSC code FE","No","Yes","Yes"),
+                           c("Industry FE","No","No","Yes"),
+                           c("Controls","No","No","No")), 
+          style="qje",
+          notes.align = "l",
+          notes=" (i) Each observation is a project-quarter, (ii) Sample restricted to firms that receive only one type of contract (small or large, but not both)",
+          type="html",
+          header=F)
 
 
 
