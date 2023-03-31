ca7489b6836b864e7e19771c47bd221a5c5dd4db
Vibhuti Dhingra <47764977+vibhuti6@users.noreply.github.com>
2023-03-30 20:46:29 -0400

    minor

--- codes/project_level_delays.R
+++ codes/project_level_delays.R
@@ -11,13 +11,19 @@ projects_to_keep=fread(paste0(data_folder,'projects_to_keep.csv'))
 full_df=fread('/Users/vibhutid_admin/Dropbox/data_quickpay/qp_data/qp_data_fy10_to_fy18.csv')
 
 full_df_tr=full_df[,c('contract_award_unique_key',
+                      'contracting_officers_determination_of_business_size_code',
+                      'period_of_performance_start_date',
                       'period_of_performance_current_end_date',
-                      'action_date')]
+                      'action_date',
+                      'naics_code',
+                      'product_or_service_code')]
 
 full_df_tr=setDT(full_df_tr)
 full_df_tr[,action_date:=as.Date(action_date)]
 full_df_tr[,period_of_performance_current_end_date:=
              as.Date(period_of_performance_current_end_date)]
+full_df_tr[,period_of_performance_start_date:=
+             as.Date(period_of_performance_start_date)]
 full_df_tr=full_df_tr[order(contract_award_unique_key,
                             action_date)]
 
@@ -28,7 +34,8 @@ full_df_tr=setDT(as.data.frame(full_df_tr) %>%
   group_by(contract_award_unique_key) %>%
   dplyr::mutate(
     initial_end_date = dplyr::first(period_of_performance_current_end_date),
-    eventual_end_date = dplyr::last(period_of_performance_current_end_date)
+    eventual_end_date = dplyr::last(period_of_performance_current_end_date),
+    initial_start_date = dplyr::first(period_of_performance_start_date)
   ))
 
 full_df_tr[,delay:=as.numeric(eventual_end_date-initial_end_date)]
@@ -47,3 +54,22 @@ full_df_tr_2=subset(full_df_tr,
 mean(full_df_tr_2$positive_delay,na.rm=T)
 sd(full_df_tr_2$positive_delay,na.rm=T)
 
+#### DID ####
+
+full_df_tr_2[,treat_i:=ifelse(contracting_officers_determination_of_business_size_code=="S",
+                              1,0)]
+
+full_df_tr_2[,post_t:=ifelse(initial_start_date>as.Date('2011-04-27'),
+                              1,0)]
+
+full_df_tr_2[,wins_delay:=Winsorize(delay,na.rm=T)]
+baseline_reg=felm(wins_delay~treat_i+
+                    post_t:treat_i+
+                    post_t|
+                    product_or_service_code+naics_code| # no fixed effects
+                    0| # no IV
+                    contract_award_unique_key, # clustered at project level
+                  data=full_df_tr_2, 
+                  exactDOF = TRUE, 
+                  cmethod = "reghdfe")
+tidy(baseline_reg)
