62d7d505637c1c2a9b28f29383f9075e1fe90bf4
Vibhuti Dhingra <vibhutidhingra@Vibhutis-MacBook-Air.local>
2026-04-09 20:24:14 -0400

    NOTE: codes updated to align with latest R packages

--- replication_codes_v1/code/file_paths.R
+++ replication_codes_v1/code/file_paths.R
@@ -3,4 +3,4 @@
 input_path='~/Dropbox/data_quickpay/qp_replication/input/'
 working_path='~/Dropbox/data_quickpay/qp_replication/working/'
 data_folder='~/Dropbox/data_quickpay/qp_data/'
-output_folder='~/Desktop/Research/QuickPay/Data-and-code/replication_codes_v1/output/'
+output_folder='~/Desktop/Research/QuickPay_Data-and-code/replication_codes_v1/output/'
